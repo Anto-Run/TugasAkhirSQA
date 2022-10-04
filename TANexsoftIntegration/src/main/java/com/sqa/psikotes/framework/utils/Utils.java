1ba65a95e0b3f67e3638a4d71286c53d2fe4ac7d
Runanto <60428139+Anto-Run@users.noreply.github.com>
2022-10-04 11:50:40 +0700

    updata utils

--- TANexsoftIntegration/src/main/java/com/sqa/psikotes/framework/utils/Utils.java
+++ TANexsoftIntegration/src/main/java/com/sqa/psikotes/framework/utils/Utils.java
@@ -90,4 +90,15 @@ public class Utils {
 	}
 	
 	
+	public static long getFileSize(String path) {
+		long fileSize = 0l;
+		File f = new File(path);
+		if(f.exists()){
+			fileSize = f.length();
+		}else{
+			fileSize = 0l;
+		}
+		return fileSize;
+	}
+	
 }
